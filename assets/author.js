892fbec2644c809cfe165df2aab7cbfe6c7f5eaa
jay <hejunjie@zmit.cn>
2020-11-11 19:35:15 +0800

    fixed:增加了作者区分

---
+++ assets/author.js
@@ -0,0 +1,18 @@
+var authors = {
+    1: {
+        "no": 1,
+        "name": "JayHe"
+    },
+    2: {
+        "no": 1,
+        "name" : "ExodusHT"
+    }
+};
+
+function getAuthor(no, key) {
+    if(key == undefined){
+        key = 'name';
+    }
+
+    return authors[no][key];
+}
