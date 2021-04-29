756beb4fdbe25e543e1083dd8c9f76058c59b623
jay <hejunjie@zmit.cn>
2021-04-29 12:15:58 +0800

    fix:site

---
+++ siteMapGenerate.py
@@ -0,0 +1,21 @@
+---
+layout: null
+---
+#!/usr/bin/python
+
+
+hosts = ["github-site-map.txt", "https://phpzlc.github.io"], ['gitee-site-map.txt', "https://phpzlc.gitee.io"]
+
+urls = [
+           "",
+           {% for post in site.posts %}
+           "{{ post.url }}"{% unless forloop.last %},{% endunless %}
+           {% endfor %}
+       ]
+
+for host in hosts:
+   content = ""
+   for url in urls:
+      content += host[1] + url + '\n'
+   f = open(host[0],'w')
+   f.write(content)
