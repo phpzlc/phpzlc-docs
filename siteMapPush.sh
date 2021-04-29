5a31ec3cbd9f977e1a7dcbc8579a7188b18d4847
jay <hejunjie@zmit.cn>
2021-04-29 16:20:28 +0800

    fix:demo

--- siteMapPush.sh
+++ siteMapPush.sh
@@ -6,6 +6,14 @@ function sitemap()
 
     echo $2 >> $1
 
+    echo $2/module/ >> $1
+
+    echo $2/about/ >> $1
+
+    echo $2/member/ >> $1
+
+    echo $2/faqs/ >> $1
+
     find _site/blog -name '*.html' -type f -print0 | while IFS= read -r -d $'\0' file;
         do echo ${file/_site/$2} >> $1
     done
@@ -20,6 +28,7 @@ sitemap sitemap.txt https://phpzlc.github.io
 sitemap sitemap-gitee.txt https://phpzlc.gitee.io
 
 curl -H 'Content-Type:text/plain' --data-binary @sitemap.txt "http://data.zz.baidu.com/urls?site=https://phpzlc.github.io&token=HvZcn9CwVs2MMMVe"
+
 curl -H 'Content-Type:text/plain' --data-binary @sitemap-gitee.txt "http://data.zz.baidu.com/urls?site=https://phpzlc.gitee.io&token=HvZcn9CwVs2MMMVe"
 
 
