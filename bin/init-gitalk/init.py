905e161b1bc057413d04c66975abe05a8fc745f3
jay <hejunjie@zmit.cn>
2021-06-25 00:30:49 +0800

    添加评论功能

--- bin/init-gitalk/init.py
+++ bin/init-gitalk/init.py
@@ -75,8 +75,8 @@ def init_gitalk(session, not_initialized):
 
 def main():
     # 暂停5分钟，主要是为了等待 vercel 编译新的文章
-#     print('sleep 300s for waiting hugo build...')
-#     time.sleep(300)
+    print('sleep 300s for waiting hugo build...')
+    time.sleep(30)
     session = requests.Session()
     session.auth = (username, token)
     session.headers = {
